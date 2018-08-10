3661a3907bdf20b7482dabfa13135c93e55ad852
Paulo Ancheta <paulo.ancheta@gmail.com>
2018-08-10 13:07:07 -0700

    be able to accept optional params

--- bamboohr/api.rb
+++ bamboohr/api.rb
@@ -1,10 +1,15 @@
 module Bamboohr
   VERSION = "v1"
   class Api
-    def self.call(endpoint)
-      HTTP.basic_auth(user: ENV["BAMBOO_API_KEY"], pass: "x").get(
-        "https://api.bamboohr.com/api/gateway.php/unbounce/#{VERSION}/#{endpoint}"
-      )
+    def self.call(endpoint, params:)
+      full_uri = "https://api.bamboohr.com/api/gateway.php/unbounce/#{VERSION}/#{endpoint}"
+      http = HTTP.basic_auth(user: ENV["BAMBOO_API_KEY"], pass: "x")
+
+      if params
+        http.get(full_uri, params: params)
+      else
+        http.get(full_uri)
+      end
     end
   end
 end
